dc524503e87eda0304eb51e818df83b14a2609ed
Roman Navarro <ronnoverro@gmail.com>
2023-01-06 22:42:58 -0800

    panicking

--- client/src/components/UploadImage.js
+++ client/src/components/UploadImage.js
@@ -21,10 +21,40 @@ function UploadImage(props) {
       <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
           <li class="breadcrumb-item"><a href="/">Albums</a></li> {/* <- we modify here */}
-          <li class="breadcrumb-item active" aria-current="page">Upload Image</li>
+          <li class="breadcrumb-item active" aria-current="page">
+            Upload Image
+          </li>
         </ol>
       </nav>
-      <div>Upload Image for album {albumDetail.name}</div>
+
+      {/* THIS STUFF PASTED */}
+      <div className="row m-1">
+        {/* CRAZY: WITHOUT ADDING THE "albumDetail.images &&" FIRST, WE GET ERROR:
+        TypeError: albumDetail.images is undefined. HOW?? */}
+        {albumDetail.images &&
+          albumDetail.images.map((image, index) => (
+            <div className="card col-md-3 mb-2">
+              <div className="card-header bg-white">
+                <span>{image}</span>
+                <button
+                  type="button"
+                  className="btn btn-danger float-end"
+                  onClick={() => handleDelete(albumId, image)}
+                >
+                  <i class="fas fa-backspace"></i>
+                </button>
+              </div>
+              <div className="card-body p-1">
+                <img
+                  style={{ width: "100%", maxHeight: "180px" }}
+                  class="img-thumbnail"
+                  src={`http://localhost:5000/${image}`}    
+                  // HERE IS HOW WE GET THE IMAGE IN ORDER TO PASTE IT. 
+                />
+              </div>
+            </div>
+          ))}
+      </div>
     </Fragment>
   )
 }
